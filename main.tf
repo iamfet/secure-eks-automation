8d000414aade171e2321741d9a372e757575b0c5
iamfet <fetuyee@gmail.com>
2025-06-27 10:03:45 +0000

    feat: add eks and vpc modules

---
+++ main.tf
@@ -0,0 +1,105 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+#VPC Cluster
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = " ~> 5.21"
+
+  name            = "${var.project_name}-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnets_cidr
+  public_subnets  = var.public_subnets_cidr
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared" # Tags required for EKS to discover subnets
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                                = 1 # Identifies this subnet for external load balancers
+  }
+
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared"
+    "kubernetes.io/role/internal_elb"                       = 1 # Identifies this subnet for internal services
+  }
+
+}
+
+#EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.36"
+
+  cluster_name    = "${var.project_name}-eks-cluster"
+  cluster_version = var.cluster_version
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  cluster_endpoint_public_access = true
+
+  create_cluster_security_group = false
+  create_node_security_group    = false
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  # Set authentication mode to API
+  authentication_mode = "API"
+
+  # Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    admin = {
+      principal_arn = aws_iam_role.external-admin.arn
+      username      = "admin"
+      type          = "STANDARD"
+      access_scope = {
+        type = "cluster"
+      }
+    }
+
+    developer = {
+      principal_arn = aws_iam_role.external-developer.arn
+      username      = "developer"
+      type          = "STANDARD"
+      access_scope = {
+        type       = "namespace"
+        namespaces = ["online-boutique"]
+      }
+    }
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      instance_types = ["m5.xlarge"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+    }
+  }
+  tags = {
+    environment = "development"
+    application = "${var.project_name}"
+  }
+
+}
