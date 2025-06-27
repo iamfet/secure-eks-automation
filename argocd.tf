47d6e141a6d48da825cdc2a61451d7ed3e0fec88
iamfet <fetuyee@gmail.com>
2025-06-27 10:38:14 +0000

    feat: add argocd

---
+++ argocd.tf
@@ -0,0 +1,22 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "8.0.17"
+  create_namespace = true
+  namespace        = "argocd"
+  depends_on       = [module.eks, module.vpc]
+}
