c16a74f686f6993a185c1499b8fd68fb47dc7634
iamfet <fetuyee@gmail.com>
2025-06-27 10:15:48 +0000

    feat: add eks blueprints addons

--- main.tf
+++ main.tf
@@ -103,3 +103,34 @@ module "eks" {
   }
 
 }
+
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.21"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  enable_cluster_autoscaler           = true
+  cluster_autoscaler = {
+    set = [
+      {
+        name  = "extraArgs.scale-down-unneeded-time"
+        value = "2m"
+      },
+      {
+        name  = "extraArgs.skip-nodes-with-local-storage"
+        value = false
+      },
+      {
+        name  = "extraArgs.skip-nodes-with-system-pods"
+        value = false
+      }
+    ]
+  }
+
+}
